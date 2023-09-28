aac385b810c3ef848583ce2567093aa9113e6307
Alex Körner <alex.koerner@gmx.net>
2023-09-28 11:34:17 +0200

    WEB-5841 minor config changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path, { resolve } from 'path'
+// import path, { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
